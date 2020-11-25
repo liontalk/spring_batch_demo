7e72952a7a3f8c949f2ca1c3cb6a862212e89555
zhouxiaoguai <liontalk@163.com>
2020-11-25 10:50:16 +0800

    整合swagger

--- file2oracle/src/main/java/cn/liontalk/file2oracle/config/Swagger2Config.java
+++ file2oracle/src/main/java/cn/liontalk/file2oracle/config/Swagger2Config.java
@@ -1,8 +1,42 @@
 package cn.liontalk.file2oracle.config;
 
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 
 @Configuration
+@EnableSwagger2
 public class Swagger2Config {
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                //为当前包路径
+                .apis(RequestHandlerSelectors.basePackage("cn.liontalk.file2oracle.controller"))
+                .paths(PathSelectors.any())
+                .build();
+               // .globalOperationParameters(pars);
+    }
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                //页面标题
+                .title("SpringBatch_Demo")
+                //创建人
+                .contact(new Contact("liontalk", "", "zhouzhe@guwodata.com"))
+                //版本号
+                .version("1.0")
+                //描述
+                .description("API 描述")
+                .build();
+    }
 }
--- file2oracle/src/main/java/cn/liontalk/file2oracle/controller/File2OracleController.java
+++ file2oracle/src/main/java/cn/liontalk/file2oracle/controller/File2OracleController.java
@@ -3,13 +3,15 @@ package cn.liontalk.file2oracle.controller;
 
 import cn.liontalk.file2oracle.entity.File2OracleEntity;
 import cn.liontalk.file2oracle.service.File2OracleService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/file2oracle")
+@Api(value = "File2OracleController", tags = {"文件到Oracle数据库"})
 public class File2OracleController {
 
 
@@ -17,8 +19,9 @@ public class File2OracleController {
     @Autowired
     private File2OracleService file2OracleService;
 
-    @RequestMapping(value = "/getById/{id}")
-    public File2OracleEntity  getFile2OracleEntityById(@PathVariable("id") String id){
+    @GetMapping(value = "/getById")
+    @ApiOperation(value = "测试API,查看数据库是否可以查询数据")
+    public File2OracleEntity  getFile2OracleEntityById(@ApiParam(value = "查询详情显示ID") @RequestParam(value = "id") String id){
         File2OracleEntity oracleEntity  = file2OracleService.getFile2OracleEntityById(id);
         return oracleEntity;
     }
